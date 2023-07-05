cdbcc018ebaad4c069f3e3528de8709c757e43a6
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2023-07-05 23:46:11 +0200

    commit

--- src/edu/kit/informatik/Dome.java
+++ src/edu/kit/informatik/Dome.java
@@ -5,14 +5,6 @@ public class Dome extends Gameobject {
     /**
      * Represents a build object type.
      *
-     * <p>
-     * This variable is used to specify the type of a build object. It is of type {@link BuildObject} and is marked as
-     * <code>private</code>, <code>static</code>, and <code>final</code>.
-     * </p>
-     *
-     * <p>
-     * The value assigned to this variable is {@link BuildObject#DOME}, indicating that the build object type is a "dome".
-     * </p>
      *
      * @see BuildObject
      */
